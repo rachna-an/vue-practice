11045662f233db76e623625cde60e9acb18096ba
Rachna-An <anrachna11@gmail.com>
2025-07-09 23:21:39 +0700

    init project

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<!-- Parent -->
+<template>
+  <ul class="list-disc">
+    <ToDoItem
+      v-for="task in tasks"
+      :key="task.id"
+      :title="task.title"
+      :isCompleted="task.done"
+      @toggle-complete="toggleTaskCompletion(task.id)"
+    />
+  </ul>
+</template>
+
+<script setup>
+  import { reactive } from 'vue'
+  import ToDoItem from './components/ToDoItem.vue'
+
+  const tasks = reactive([
+    { id: 1, title: 'Code Review', done: false },
+    { id: 2, title: 'Write documentation', done: true },
+    { id: 3, title: 'Fix bugs', done: false },
+  ])
+
+  const toggleTaskCompletion = (id) => {
+    const task = tasks.find((t) => t.id === id)
+    if (task) task.done = !task.done
+  }
+</script>
---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,21 @@
+<!-- Component: ToDoItem.vue -->
+<template>
+  <li>
+    <input type="checkbox" :checked="isCompleted" @change="emitToggle" class="cursor-pointer" />
+    <label :class="{ 'line-through text-gray-400': isCompleted }" class="pl-3">
+      {{ title }}
+    </label>
+  </li>
+</template>
+
+<script setup>
+  defineProps({
+    title: String,
+    isCompleted: Boolean,
+  })
+
+  const emit = defineEmits(['toggle-complete'])
+  function emitToggle() {
+    emit('toggle-complete')
+  }
+</script>
